12786dde02c7c8091dde76ebc230ddde0e7712a1
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-08-24 14:36:59 +0200

    changed title for home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container'
 const HomePage = () => {
 	return (
 		<Container className="py-3">
-			<h1>Welcome!</h1>
+			<h1>Welcome to The Movie DB</h1>
 		</Container>
 	)
 }
