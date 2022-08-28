a315826f02f0a75d296c69aa4affa7ed8c50cff0
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-08-28 12:28:26 +0200

    added row and col

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,31 +1,37 @@
 import { Button } from "react-bootstrap"
 import { Container } from "react-bootstrap"
 import { Card } from "react-bootstrap"
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import { Link } from "react-router-dom"
 
 const MovieDetails = ({ movies }) => {
     return (
         <Container>
-            {movies.results.map((movie) => 
-                <Card key={movie.id} className='mb-3'>
-                    <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} />
-                    <Card.Body>
-                            <div>
-                                <span>Released: {movie.release_date} </span>
-                            </div>
-                            <div>
-                                <span>Average rating: {movie.vote_average}</span>
-                            </div>
-                            <div>
+            <Row>
+                {movies.results.map((movie) => 
+                    <Col lg={3} md={4} sm={6} key={movie.id}>
+                        <Card className='mb-3'>
+                            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} />
+                            <Card.Body>
+                                    <div>
+                                        <span>Released: {movie.release_date} </span>
+                                    </div>
+                                    <div>
+                                        <span>Average rating: {movie.vote_average}</span>
+                                    </div>
+                                    <div>
                                         <Button
                                             as={Link}
                                             to={`/movie/${movie.id}`}
                                             >Read more
                                         </Button>
-                            </div>
-                    </Card.Body>
-                </Card> 
-            )}
+                                    </div>
+                            </Card.Body>
+                        </Card> 
+                    </Col>
+                )}
+            </Row>
         </Container>
     )
 }
